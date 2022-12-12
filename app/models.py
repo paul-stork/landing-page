82545e490f334510fc54ab5149a0f46392b8c57f
Paul Stork <paul.michael.stork@gmail.com>
2022-12-11 21:11:31 -0500

    Inital add of first crypto plot!

--- app/models.py
+++ app/models.py
@@ -1,2 +1,15 @@
 from .extensions import db
 
+class daily_crypto_data(db.Model):
+    __tablename__ = "daily_crypto_data"
+    __table_args__ = {"schema":"crypto_data"}
+    id = db.Column(db.Integer, primary_key=True)
+    exchange_id = db.Column(db.VARCHAR(5))
+    rate_close = db.Column(db.Numeric(19, 9))
+    rate_high = db.Column(db.Numeric(19, 9))
+    rate_low = db.Column(db.Numeric(19, 9))
+    rate_open = db.Column(db.Numeric(19, 9))
+    time_close = db.Column(db.DateTime(timezone=True))
+    time_open = db.Column(db.DateTime(timezone=True))
+    time_period_end = db.Column(db.DateTime(timezone=True))
+    time_period_start = db.Column(db.DateTime(timezone=True))
